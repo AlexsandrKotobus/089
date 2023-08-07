c99dcfa1a7d9614a5ff4254085995058e559ef78
Julia <alexsandr.kotobus@gmail.com>
2023-08-08 01:32:57 +0300

    Fix Issue #{number}: {description}
    R+: {reviewer}

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,9 +1,12 @@
 ﻿import React  from 'react';
+import { Button } from 'reactstrap';
 import './post-status-filter.css'
 
 const PostStatusFilter = ()=>{
     return (
         <div className='btn-group'>
+            {/* использование компонента со стилем bootstrap */}
+            <Button color='info'>ALL</Button>
             <button type='button' className='btn btn-info'>Все</button>
             <button type='button' className='btn btn-outline-secondary'>Понравилось</button>            
         </div>
